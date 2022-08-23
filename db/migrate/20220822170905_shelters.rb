a6c2f1495fc54b0d1b14f3c1297dd9d2b2d08f5b
Justin Myers <106842164+jlmyers14@users.noreply.github.com>
2022-08-23 12:58:03 -0600

    Add files via upload

---
+++ db/migrate/20220822170905_shelters.rb
@@ -0,0 +1,12 @@
+class Shelters < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shelters do |t|
+      t.string :name
+      t.string :image_url
+      t.string :location
+      t.string :hours
+      t.boolean :no_kill?
+      t.integer :shelter_id
+    end
+  end
+end
