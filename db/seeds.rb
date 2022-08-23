0232ebd13dd0d3a78e4d4bbf49bfa10be439d69e
Karter <105955246+kdliving14@users.noreply.github.com>
2022-08-23 12:31:52 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,30 @@
 puts "Deleting ..."
 Parent.destroy_all
-
-puts "Deleting ..."
 Shelter.destroy_all
-
-puts "Deleting ..."
 Pets.destroy_all
 
 puts "🌱 Seeding spices..."
 
 puts "seeding foster_parents"
+p1 = Parent.create(
+    image_url: Faker::LoremFlickr.image(size: "300x300", search_terms:['self']),
+    name: "TEST",
+    service_years: rand(0..10),
+    preferences: "dogs",
+    budget: rand(100..2000),
+    hours_at_home: rand(1..40),
+    living_situation: "House",
+    current_num_pets: rand(0..5),
+    pet_confiscation?: false,
+    vet_provider: "Pets Best",
+    willing_6m_follow_up?: true,
+    give_pet_back?: false,
+    donated_amt: rand(100..300),
+    reoccuring?: true,
+    gift?: true,
+    gift_in_honor_of: "parent"
+    )
+
 5.times do
 
     Parent.create(
@@ -63,7 +78,7 @@ s3 = Shelter.create(name: "California Animal Shelter", image_url: "https://www.s
     hours: "9-6", no_kill?: true, shelter_id: 3)
 
 # Seed your database here
-p1 = Pets.create(shelter_id: 1, parent_id: 1, name: "Lucky", image_url: "https://www.nationalgeographic.com/animals/mammals/facts/domestic-dog", species: "dog", hunger: 5, thirst: 5, fun: 5, hygiene: 5, exhaustion: 5, temperament: "cute", recommendations: "no kids", adopt_status: "not started", medical: "skin rashes", age: 6, gender: "Male", breed: "mutt", price: 120)
-p2 = Pets.create(shelter_id: 2, parent_id: 2, name: "Plucky", image_url: "https://www.nationalgeographic.com/animals/mammals/facts/domestic-dog", species: "bird", hunger: 5, thirst: 5, fun: 5, hygiene: 5, exhaustion: 5, temperament: "cute", recommendations: "no kids", adopt_status: "not started", medical: "n/a", age: 1, gender: "Male", breed: "parakeet", price: 130)
+p1 = Pets.create(shelter_id: s1.id, parent_id: p1.id, name: "Lucky", image_url: "https://www.nationalgeographic.com/animals/mammals/facts/domestic-dog", species: "dog", hunger: 5, thirst: 5, fun: 5, hygiene: 5, exhaustion: 5, temperament: "cute", recommendations: "no kids", adopt_status: "not started", medical: "skin rashes", age: 6, gender: "Male", breed: "mutt", price: 120)
+p2 = Pets.create(shelter_id: s2.id, parent_id: p1.id, name: "Plucky", image_url: "https://www.nationalgeographic.com/animals/mammals/facts/domestic-dog", species: "bird", hunger: 5, thirst: 5, fun: 5, hygiene: 5, exhaustion: 5, temperament: "cute", recommendations: "no kids", adopt_status: "not started", medical: "n/a", age: 1, gender: "Male", breed: "parakeet", price: 130)
 
 puts "✅ Done seeding!"
