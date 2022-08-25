f991cfcec5dae93d4fd0fd2274a94cd77f2b4f36
kgicheha <55036976+kgicheha@users.noreply.github.com>
2022-08-25 11:54:33 -0700

    Update parents_controller.rb

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -45,13 +45,13 @@ class ParentsController < ApplicationController
     end
 
     def parent_params
-        {
+         {
             image_url: params[:image_url], name: params[:name], service_years: params[:service_years],
             preferences: params[:preferences], budget: params[:budget], hours_at_home: params[:hours_at_home],
             living_situation: params[:living_situation], current_num_pets: params[:current_num_pets],
-            pet_confiscation?: params[:pet_confiscation?], vet_provider: params[:vet_provider],
-            willing_6m_follow_up?: params[:willing_6m_follow_up?], give_pet_back?: params[:give_pet_back?],
-            donated_amt: params[:donated_amt], reoccuring?: params[:reoccuring?], gift?: params[:gift?],
+            pet_confiscation: params[:pet_confiscation], vet_provider: params[:vet_provider],
+            willing_6m_follow_up: params[:willing_6m_follow_up], give_pet_back: params[:give_pet_back],
+            donated_amt: params[:donated_amt], reoccuring: params[:reoccuring], gift: params[:gift],
             gift_in_honor_of: params[:gift_in_honor_of]
         }
     end
